8c281826545f2630448334d7f1f663cf9214db23
Cien <vnk22012002@gmail.com>
2023-04-27 19:56:35 +0700

    Update day 27/4/23

--- js/main.js
+++ js/main.js
@@ -1,10 +1,69 @@
-// check
+//Chuyển icon Dark or Light
+document.getElementById("moon").addEventListener("click", function () {
+    document.getElementById("moon").classList.remove("showDarkandLight");
+    document.getElementById("moon").classList.add("closeDarkandLight");
+    document.getElementById("sun").classList.remove("closeDarkandLight");
+    document.getElementById("sun").classList.add("showDarkandLight");
+})
+
+document.getElementById("sun").addEventListener("click", function () {
+    document.getElementById("sun").classList.remove("showDarkandLight");
+    document.getElementById("sun").classList.add("closeDarkandLight");
+    document.getElementById("moon").classList.remove("closeDarkandLight");
+    document.getElementById("moon").classList.add("showDarkandLight");
+})
+/**
+ * chuyển đổi theme cho trang web
+ * @localStorage.lastname {string}: biến lưu giá trị cho biết hiện tại trang web đang hiển thị dark mode hay light mode. y là dark mode, không phải y là light mode
+ */
+var isDark = true;
+document.getElementById('moon').onclick = function () {
+    switchDarkTheme();
+}
+document.getElementById('sun').onclick = function () {
+    switchDarkTheme();
+}
+function switchDarkTheme() {
+    if (localStorage.dark != 'y') {
+        localStorage.setItem("dark", "y");
+        document.documentElement.setAttribute('data-theme', 'dark');
+        return;
+    }
+    localStorage.setItem("dark", "n");
+    document.documentElement.setAttribute('data-theme', 'light');
+}
+
+
+
+//Lists navbar icon
+document.getElementById("showListNavbar").addEventListener("click", function () {
+    document.getElementById("closeListNavbar").classList.add("show");
+    document.getElementById("showListNavbar").classList.remove("show");
+    document.getElementById("showListNavbar").classList.add("close");
+    document.getElementById("closeListNavbar").classList.remove("close");
+
+})
+
+document.getElementById("closeListNavbar").addEventListener("click", function () {
+    document.getElementById("showListNavbar").classList.add("show");
+    document.getElementById("closeListNavbar").classList.remove("show");
+    document.getElementById("closeListNavbar").classList.add("close");
+    document.getElementById("showListNavbar").classList.remove("close");
+
+})
+
+
+// Overlay search
 document.getElementById("showOverlay").addEventListener("click", function () {
     document.getElementById("search").classList.add("show");
 })
-document.getElementById("close").addEventListener("click", function () {
+document.getElementById("closeOverlay").addEventListener("click", function () {
     document.getElementById("search").classList.remove("show");
 })
+
+
+
+
 /**
  * Công Nhựt
  */
@@ -69,20 +128,4 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
-/**
- * chuyển đổi theme cho trang web
- * @localStorage.lastname {string}: biến lưu giá trị cho biết hiện tại trang web đang hiển thị dark mode hay light mode. y là dark mode, không phải y là light mode
- */
-var isDark = false;
-document.getElementById('moon').onclick = function () {
-    switchDarkTheme();
-}
-function switchDarkTheme() {
-    if (localStorage.dark != 'y') {
-        localStorage.setItem("dark", "y");
-        document.documentElement.setAttribute('data-theme', 'dark');
-        return;
-    }
-    localStorage.setItem("dark", "n");
-    document.documentElement.setAttribute('data-theme', 'light');
-}
+
